21e42e0152dab7ab5a6518705f3d4b5a47368eb6
Winston J Lamus <138252771+WinstonLesuT@users.noreply.github.com>
2024-01-20 01:46:13 +0000

    landing page

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// const myStyle = {
+// 	'background-color':'#e3f2fd'
+// }
+
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">What to do</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					{/* crea un enlace al Sing up y al Log in */}
+					<Link to="/log-in">
+						<button className="btn btn-light me-1">Log in</button>
 					</Link>
+					<Link to="/demo">
+						<button className="btn btn-primary">Sing up</button>
+					</Link>
+
 				</div>
 			</div>
 		</nav>
