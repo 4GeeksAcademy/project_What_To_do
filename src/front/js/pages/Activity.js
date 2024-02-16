a758cbeb26ee3de68c0fea1873bdc33e769a7935
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2024-02-16 21:51:39 +0000

    activityveiw

--- src/front/js/pages/Activity.js
+++ src/front/js/pages/Activity.js
@@ -1,27 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-const API_URL = `https://api.foursquare.com/v3/places/search`;
+// const API_URL = `https://api.foursquare.com/v3/places/search`;
 const API_KEY = `fsq36n4VDirwVjuExOsAhVU+3oLweispAYAg5bmsTeT9gUg=`;
 
 export const Activity = () => {
     const [result, setResult] = useState({});
     const { id } = useParams();
-    
-    return (
-        
-        <div className="X">
+    const [requestStatus, setRequestStatus] = useState('pending');
+    const [photosURL, setPhotosURL] = useState([]);
+  
+    const getDetails = async () => {
+        let response = await fetch (`https://api.foursquare.com/v3/places/${id}`, {
+            method: "GET",
+            headers: {Authorization: API_KEY}
+        })
+        let photoResponse = await fetch (`https://api.foursquare.com/v3/places/${id}/photos`, {
+            method: "GET",
+            headers: {Authorization: API_KEY}
+        })
+       
+        if(!response.ok){
+            setRequestStatus('error')
+        } else if (response.ok && photoResponse.ok){
+            let data = await response.json();
+            let photoData = await photoResponse.json()
             
-            {id}
-            
-        
-        <h1>
-            
-        </h1>
-        <h3>
-            
-        </h3>
+            setResult(data)
+            setPhotosURL(photoData)
+            setRequestStatus("done");
+        }
+    }
 
-        </div>
-    )
+    useEffect(() => {
+        getDetails()
+    },[]) 
+
+    switch(requestStatus) {
+        case 'pending':
+          return (
+            <div>
+                loading...
+            </div>
+          )
+        case 'done':
+          return (
+           
+            <div className="container">
+                
+                <div className="row justify-content-center">
+                    <p></p>  
+                    <p>{result.categories.name}</p>
+                    {id}
+                   
+                    <header className="py-5 bg-light border-bottom mb-4">
+                        <div class="container">
+                            <div className="text-center my-5">
+                                <h1 className="fw-bolder">{result.name}</h1>
+                                <p className="lead mb-0">{result.location.formatted_address}</p>
+                            </div>
+                        </div>
+                    </header>
+                    <div id="carouselExampleIndicators" class="carousel slide col-8">
+                            <div className="carousel-indicators">
+                                {photosURL.map((item, index) => (
+                                    <button
+                                        key={index}
+                                        type="button"
+                                        data-bs-target="#carouselExampleIndicators"
+                                        data-bs-slide-to={index}
+                                        className={index === 0 ? 'active' : ''}
+                                        aria-current={index === 0 ? 'true' : 'false'}
+                                        aria-label={`Slide ${index + 1}`}
+                                    ></button>
+                                ))}
+                            </div>
+                            <div className="carousel-inner">
+                                {photosURL.map((item, index) => (
+                                    <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+                                        <img src={`${item.prefix}800x400${item.suffix}`} className="d-block w-40" alt="" />
+                                    </div>
+                                ))}
+                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>    
+
+
+                </div>
+
+
+            
+          )
+         
+        default:
+            return(
+                <div>
+                    an error ocurred while loading data
+                </div>
+            )
+          
+      }
+
+    
 }
